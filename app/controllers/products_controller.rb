4c2e95b72b5af4c2fdde123e97c169ac4c14c898
Cameron Webster <cameron.webster@gmail.com>
2015-02-10 14:57:39 -0700

    user can view product show page with reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
+    @company = Company.find(params[:company_id])
+    @reviews = @product.reviews
   end
 
 end
